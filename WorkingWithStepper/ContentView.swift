304cda83fda3b2831457bffd9588cccc349fc2de
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-07 10:55:53 +0300

    Initial Commit

---
+++ WorkingWithStepper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkingWithStepper
+//
+//  Created by ramil on 07.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkingWithStepper/WorkingWithStepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithStepperApp.swift
+//  WorkingWithStepper
+//
+//  Created by ramil on 07.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithStepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
