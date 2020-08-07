950a8dc20d76a2dde47cbcb79f53b718946501e1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-07 11:03:47 +0300

    init project

--- WorkingWithStepper/ContentView.swift
+++ WorkingWithStepper/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var stepperValue = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Stepper")
+                .font(.largeTitle)
+            
+            Stepper(value: $stepperValue, in: 1...9) {
+                Text("Stepper")
+            }
+            // Hide label
+            //.labelsHidden()
             .padding()
+            
+            HStack {
+                ForEach(1...stepperValue, id: \.self) { i in
+                    Image(systemName: "hand.thumbsup")
+                        .font(.largeTitle)
+                        .foregroundColor(.purple)
+                }
+            }
+            
+            Text("Stepper Value \(stepperValue)")
+        }
     }
 }
 
